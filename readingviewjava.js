6fa3ec3f46b5257f29a81b2db4b1361fedde4e69
mgb18 <mgb18@pitt.edu>
2014-04-06 21:09:14 -0400

    To Upload to Main Site Tomorrow
    Pending a few questions, here is the fixed version of everything we've
    been working on.  Everything should be good, but I'll be editing and
    changing things throughout the week as well, but just so we have
    something better up on the site.

---
+++ readingviewjava.js
@@ -0,0 +1,15 @@
+window.onload = init
+
+function init() {
+    var ul = document.getElementsByTagName("li");
+    li[0].addEventListener('click', show_hide, false);
+}
+
+function show_hide() {
+    var ul = document.getElementByTagName("ul");
+    if (p.style.display == "none") {
+        p.style.display = "block";
+    } else {
+        p.style.display = "none";
+    }
+}
